e920c5f1abcd3f5aaa179439fe081f2d51c0e451
kyouka <magrookr014@gmil.com>
2026-02-19 15:25:27 +0900

    kirikaebotan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import  FloatingActions from "@/app/under botton/page";
 import Calendar from "@/components/Calendar";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div >
+   export default function Home() {
+     return (
+       <div >
       <main >
+      <FloatingActions/>
         <Calendar />
       </main>
     </div>
